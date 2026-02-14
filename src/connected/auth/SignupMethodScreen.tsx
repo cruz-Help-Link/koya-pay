5b2f6a61d1711109bb4374dcdf8af2856b248af1
cruz-Help-Link <itzdonnacruz00@gmail.com>
2026-02-14 11:09:53 -0600

    Connect Verify Email screen to Sign Up
    Author: Eniola Fanegan

---
+++ src/connected/auth/SignupMethodScreen.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+import { SignupMethodScreen as SignupMethodScreenComponent } from '../../pages/SignUpMethod';
+import { useSignupStore } from '../../stores';
+
+export const SignupMethodScreen = () => {
+  const navigate = useNavigate();
+  const { setSignupMethod } = useSignupStore();
+
+  // For now, this component handles its own navigation
+  // Later we can enhance to integrate with the store
+  return <SignupMethodScreenComponent />;
+};
