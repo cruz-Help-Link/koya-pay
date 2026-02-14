22759ccc26cbdc9bef5bc8a53b8d55f748f9eb6b
cruz-Help-Link <itzdonnacruz00@gmail.com>
2026-02-14 11:10:58 -0600

    Add LoginUserScreen
    Author: Eniola Fanegan

---
+++ src/connected/auth/LoginUserScreen.tsx
@@ -0,0 +1,7 @@
+import { LoginScreen as LoginScreenComponent } from "../../screens/auth/LoginScreen";
+
+export const LoginUserScreen = () => {
+  // For now, the component handles its own state
+  // We'll need to modify it to accept callbacks later
+  return <LoginScreenComponent />;
+};
