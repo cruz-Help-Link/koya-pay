f6777761b94546e41a6788fe9ac2404ef3ba0996
cruz-Help-Link <itzdonnacruz00@gmail.com>
2026-02-21 03:44:28 -0600

    ADD index.ts for Dashboard and Onboarding

---
+++ src/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { default as Dashboard } from "./Dashboard";
+export { default as TransactionsScreen } from "./TransactionsScreen"
+export { default as BottomNavigation } from "./BottomNavigation";
+export { default as DashHeader } from "./DashHeader";
