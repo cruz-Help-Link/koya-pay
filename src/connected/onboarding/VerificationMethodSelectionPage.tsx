0138b1b41e4da0e091d75c7f60c49451d0e89bc8
cruz-Help-Link <itzdonnacruz00@gmail.com>
2026-02-21 03:42:26 -0600

    ADD VerificationMethodSelectionPage.tsx

--- src/connected/onboarding/VerificationMethodSelectionPage.tsx
+++ src/connected/onboarding/VerificationMethodSelectionPage.tsx
@@ -1,25 +1,26 @@
-import { useNavigate } from 'react-router-dom';
-import { VerificationMethodSelectionPage as VerificationMethodSelectionPageComponent } from '../../pages/onboarding';
-import { useOnboardingStore } from '../../stores';
-import type { VerificationMethod } from '../../types';
+import { useNavigate } from "react-router-dom";
+import { VerificationMethodSelectionPage as VerificationMethodSelectionPageComponent } from "../../screens/onboarding";
+import { useOnboardingStore } from "../../stores";
+import type { VerificationMethod } from "../../types";
 
 export const VerificationMethodSelectionPage = () => {
   const navigate = useNavigate();
-  const { currentStep, goToNextStep, setVerificationMethod } = useOnboardingStore();
+  const { currentStep, goToNextStep, setVerificationMethod } =
+    useOnboardingStore();
 
   const handleSelectMethod = (method: VerificationMethod) => {
     setVerificationMethod(method);
     goToNextStep();
-    navigate('/onboarding/document-upload');
+    navigate("/onboarding/document-upload");
   };
 
   const handleSkip = () => {
-    navigate('/');
+    navigate("/");
   };
 
   const handleNext = () => {
     goToNextStep();
-    navigate('/onboarding/document-upload');
+    navigate("/onboarding/document-upload");
   };
 
   return (
